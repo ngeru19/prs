319f9e83ee306b8c99f19027251ae34fad7a469d
ngeru19 <ngeru19@gmail.com>
2022-12-13 11:13:12 +0600

    adding modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header';
 import Playground from './components/Playground';
+import Rules from './components/Rules';
+import Modal from './components/Modal';
+import { useState } from 'react';
 
 function App() {
+
+  const [modalActive, setModalActive] = useState('true');
+
   return (
     <div className="App">
       <Header />
       <Playground />
+      <Modal 
+      active = {modalActive}
+      setActive = {setModalActive}
+      />
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css"
+
+export default function Modal({active}, {setActive}) {
+    <div className="modal">
+        <div className="modal__content">
+            <p className="p">Hello world</p>
+        </div>
+    </div>
+}
